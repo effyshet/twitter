1a4c34412306c25862d75f2166ba0c37621180ce
Effy <effyshet@gmail.com>
2022-10-14 16:18:32 +0000

    Twitter

--- contracts/twitter-q1.sol
+++ contracts/twitter-q1.sol
@@ -22,18 +22,41 @@ contract Twitter {
     // ----- END OF DO-NOT-EDIT ----- //
 
     function registerAccount(string calldata _name) external {
-
+        bytes memory tempEmptyStringTest = bytes(_name); 
+        require(tempEmptyStringTest.length != 0,"Name cannot be an empty string");
+        User memory _recentUser;
+        _recentUser.name = _name;
+        _recentUser.wallet = msg.sender;
+        users[msg.sender] = _recentUser;
     }
 
     function postTweet(string calldata _content) external accountExists(msg.sender) {     
+        Tweet memory _recentTweet;
+        _recentTweet.tweetId = nextTweetId;
+        _recentTweet.author = msg.sender;
+        _recentTweet.content = _content;
+        _recentTweet.createdAt = block.timestamp;
 
+        tweets[nextTweetId] = _recentTweet;
+        users[msg.sender].userTweets.push(nextTweetId);
+        nextTweetId += 1;
     }
 
     function readTweets(address _user) view external returns(Tweet[] memory) {
-
+        uint[] memory userTweetsId = users[_user].userTweets;
+        uint userTweetLength = userTweetsId.length;
+        Tweet[] memory userTweets = new Tweet[](userTweetLength);
+        uint i;
+        for(i = 0; i < userTweetLength; i++) {
+            uint tweetId = userTweetsId[i];
+            userTweets[i] = tweets[tweetId];
+        }
+        return userTweets;
     }
 
     modifier accountExists(address _user) {
+        string memory _username = users[_user].name;
+        require(bytes(_username).length != 0, "This wallet does not belong to any account.");
         _;
     }
 
