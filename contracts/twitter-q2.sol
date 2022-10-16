eaccef13c20650a5c80df3082b4f352751260023
Effy <effyshet@gmail.com>
2022-10-16 21:35:03 +0000

    stackup

--- contracts/twitter-q2.sol
+++ contracts/twitter-q2.sol
@@ -34,46 +34,77 @@ contract Twitter {
     // ----- END OF DO-NOT-EDIT ----- //
 
     // ----- START OF QUEST 1 ----- //
-    function registerAccount(string calldata _name) external {
-
+    function registerAccount(string memory _name) external {
+        require(bytes(_name).length != 0, "Name cannot be an empty string");
+        User storage _recentUser = users[msg.sender];
+        _recentUser.wallet = msg.sender;
+        _recentUser.name = _name;
+        //users[msg.sender] = _recentUser;
     }
 
     function postTweet(string calldata _content) external accountExists(msg.sender) {     
-
+        Tweet memory _recentTweet = Tweet(
+            nextTweetId,
+            msg.sender,
+            _content,
+            block.timestamp
+        );
+        tweets[nextTweetId] = _recentTweet;
+        users[msg.sender].userTweets.push(nextTweetId);
+        nextTweetId += 1;
     }
 
     function readTweets(address _user) view external returns(Tweet[] memory) {
-
+        uint[] memory userTweetsIds = users[_user].userTweets;
+        Tweet[] memory userTweets = new Tweet[](userTweetsIds.length);
+        for(uint256 i = 0; i < userTweetsIds.length; i++) {
+            userTweets[i] = tweets[userTweetsIds[i]];
+        }
+        return userTweets;
     }
 
     modifier accountExists(address _user) {
+        string memory account = users[_user].name;
+        require(bytes(account).length != 0, "This wallet does not belong to any account.");
         _;
     }
     // ----- END OF QUEST 1 ----- //
 
     // ----- START OF QUEST 2 ----- //
     function followUser(address _user) external {
-
+        users[msg.sender].following.push(_user);
+        users[_user].followers.push(msg.sender);
     }
 
     function getFollowing() external view returns(address[] memory)  {
-
+        return users[msg.sender].following;
     }
 
     function getFollowers() external view returns(address[] memory) {
-
+        return users[msg.sender].followers;
     }
 
     function getTweetFeed() view external returns(Tweet[] memory) {
-
+        Tweet[] memory allTweets = new Tweet[](nextTweetId);
+        for (uint i = 0; i < nextTweetId; i++) {
+            allTweets[i] = tweets[i];
+        }
+        return allTweets;
     }
 
     function sendMessage(address _recipient, string calldata _content) external {
-
+        Message memory _recentMessage;
+        _recentMessage.messageId = nextMessageId;
+        _recentMessage.content = _content;
+        _recentMessage.from = msg.sender;
+        _recentMessage.to = _recipient;
+        users[msg.sender].conversations[_recipient].push(_recentMessage);
+        users[_recipient].conversations[msg.sender].push(_recentMessage);
+        nextMessageId++;
     }
 
     function getConversationWithUser(address _user) external view returns(Message[] memory) {
-
+        return users[msg.sender].conversations[_user];
     }
     // ----- END OF QUEST 2 ----- //
 }
